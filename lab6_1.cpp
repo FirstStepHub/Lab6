ed1b0c97ead3d8c44f58010bff5b433d34dba876
FirstStepHub <first_yo1@hotmail.com>
2025-12-12 18:19:14 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
-using namespace std;
+using namespace std ;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+int main()
+{
+    int number ;
+    int evenCount = 0 ; 
+    int oddCount = 0 ;
+
+    cout << "Enter an integer: " ;
+    while (cin >> number && number != 0) 
+    {
+        if (number % 2 == 0) {
+            evenCount++ ; 
+        } else {
+            oddCount++ ;
+        }
+        cout << "Enter an integer: " ;
+    }
+    
+    cout << "#Even numbers = " << evenCount << endl ;
+    cout << "#Odd numbers = " << oddCount << endl ;
+    return 0 ;
 }
